fed895549e539e044bef7f8eb99e3c5bd1b61480
Shaishav Gupta <141803885+CodeCrafted7@users.noreply.github.com>
2023-09-06 21:22:54 +0530

    Add files via upload

---
+++ Java Basics/five.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class five {
+    public static void main(String[] args) {
+       int i ;
+       for(i=0;i<10;i++)
+       {
+        System.out.println("2 ^"+i+" =  "+(int)Math.pow(2,i));
+       } 
+    }
+}
