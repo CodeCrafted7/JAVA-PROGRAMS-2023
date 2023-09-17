b9c2dfba0960e4da329161c6c9eaa33193b51f99
Shaishav Gupta <141803885+CodeCrafted7@users.noreply.github.com>
2023-09-17 10:32:58 +0530

    Add files via upload

---
+++ String/q67.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+class Solution {
+    long[][] dp = new long[1000][1000];
+
+    long helper(String str, int i, int j) {
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        int mod = (int) Math.pow(10, 9) + 7;
+
+        if (i > j) {
+
+            return 0;
+        }
+        if (i == j)
+            return dp[i][j] = 1;
+
+        else if (str.charAt(i) == str.charAt(j)) {
+            return dp[i][j] = (1 + helper(str, i + 1, j) + helper(str, i, j - 1)) % mod;
+        }
+
+        else {
+            return dp[i][j] = (mod + helper(str, i + 1, j) + helper(str, i, j - 1) - helper(str, i + 1, j - 1)) % mod;
+        }
+
+    }
+
+    long countPS(String str) {
+        int n = str.length();
+        for (int i = 0; i < 1000; i++) {
+            for (int j = 0; j < 1000; j++) {
+                dp[i][j] = -1;
+            }
+        }
+        return helper(str, 0, n - 1);
+    }
+
+}
+
+public class q67 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the String : ");
+        String str = sc.next();
+        Solution s = new Solution();
+        s.countPS(str);
+
+    }
+}
