23e5effdbbc57f5d53213fc3de6ff942e86e5c45
Shaishav Gupta <141803885+CodeCrafted7@users.noreply.github.com>
2023-11-10 07:04:00 +0530

    Add files via upload

---
+++ Programs/four.java
@@ -0,0 +1,11 @@
+public class four {
+    public static void main(String[] args) {
+        String str ="Graphic era hill university";
+        String str1 = "Graphic Era Deemed To be University";
+        System.out.println(str.charAt(5));
+        System.out.println(str.codePointAt(5));
+        System.out.println(str.concat(str1));
+        System.out.println(str.length());
+        System.out.println(str.indexOf('p'));
+    }
+}
